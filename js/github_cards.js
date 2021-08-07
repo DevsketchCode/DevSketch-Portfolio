73ee6f42935f83059137a97075c3277cd6f76b87
David Oberlander <69563324+DevsketchCode@users.noreply.github.com>
2021-08-07 12:20:08 -0500

    added icons

--- js/github_cards.js
+++ js/github_cards.js
@@ -1,9 +1,9 @@
 $.ajax({
   url: "https://api.github.com/users/DevsketchCode/repos",
-  jsonp: true, 
-  method: "GET", 
-  dataType: "json", 
+  jsonp: true,
+  method: "GET",
+  dataType: "json",
   success: function(res) {
-    console.log(res)
+    //alert(res);
   }
 });
