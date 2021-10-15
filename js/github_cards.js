fb1ee3bee4df3fd2647d01f544ce62cbfffc8a59
David Oberlander <69563324+DevsketchCode@users.noreply.github.com>
2021-10-15 17:06:34 -0500

    Got github repo retrieval working

--- js/github_cards.js
+++ js/github_cards.js
@@ -1,6 +1,10 @@
 (function() {
   $(document).ready(function() {
 
+
+  });
+})();
+
     // Gets the full repo name from the data attribute and
     // fetches the data from the api.
     $('[data-github]').each(function () {
@@ -10,10 +14,45 @@
       fetch('https://api.github.com/repos/' + repo).then(function (response) {
         return response.json();
       }).then(function (response) {
+        // Retrieve repo details;
+        $(_this).find('[data-name]').text(response.name);
+        $(_this).find('[data-fullname]').text(response.full_name);
+        $(_this).find('[data-html_url]').text(response.html_url);
+        $(_this).find('[data-description]').text(response.description);
         $(_this).find('[data-forks]').text(response.forks);
         $(_this).find('[data-stars]').text(response.stargazers_count);
+        $(_this).find('[data-watchers]').text(response.watchers_count);
+        $(_this).find('[data-language]').text(response.language);
+        $(_this).find('[data-open_issues]').text(response.open_issues);
+        $(_this).find('[data-size]').text(response.size);
+        $(_this).find('[data-subscribers_count]').text(response.subscribers_count);
+        $(_this).find('[data-network_count]').text(response.network_count);
       });
     });
 
-  });
-})();
+
+    /*
+
+   
+    https://docs.github.com/en/rest/reference/activity#list-repositories-starred-by-the-authenticated-user--code-samples
+    
+    
+    "html_url": "https://github.com/octocat/Hello-World",
+    "description": "This your first repo!",
+
+    "name": "Hello-World",
+    "full_name": "octocat/Hello-World",
+
+    "language": null,
+    "forks_count": 9,
+    "stargazers_count": 80,
+    "watchers_count": 80,
+    "size": 108,
+    "default_branch": "master",
+    "open_issues_count": 0,
+    "subscribers_count": 42,
+    "network_count": 0,
+    "forks": 1,
+    "open_issues": 1,
+    "watchers": 1
+    */
