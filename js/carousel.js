66f15d200b56c8e7e2d8f83e7e8bc06f51f6dcd1
David Oberlander <david.oberlander@student.cvtc.edu>
2024-02-16 23:49:02 -0600

    Add U Learn It Java App to projects carousel

--- js/carousel.js
+++ js/carousel.js
@@ -6,16 +6,18 @@ $(document).ready(function() {
   
   // Links to images that you want in the carousel, could be relative links
   $imageLinkArray = [ 
-    "https://devsketchcode.github.io/DevSketch-Portfolio/images/BetaAirlines_Plan.jpg",
-    "https://devsketchcode.github.io/DevSketch-Portfolio/images/BetaAirlinesMVC_07_User_BookAFlight.jpg",
-    "https://devsketchcode.github.io/DevSketch-Portfolio/images/pawpering_screenshot_lg.png",
-    "https://devsketchcode.github.io/DevSketch-Portfolio/images/JavaMovieApplication.jpg"
+    "https://www.devsketch.com/images/BetaAirlines_Plan.jpg",
+    "https://www.devsketch.com/images/BetaAirlinesMVC_07_User_BookAFlight.jpg",
+    "https://www.devsketch.com/images/pawpering_screenshot_lg.png",
+    "https://www.devsketch.com/images/JavaMovieApplication.jpg",
+    "https://www.devsketch.com/images/ULearnIt_FeatureGraphic.png",
   ];
   $imageLinkDescriptionArray = [ 
     "I developed this Beta Airlines project website in ASP.NET using C#.  This particular version is using WebForms.",
     "This Beta Airlines project was developed in ASP.NET using C# taking advantage of MVC and the Entity Framework. I made it to allow general users, managers, and admins to log in with their appropriate access.",
     "For the Pawpering project, I used VB.NET to create this functional demo of a veterinary clinic CRM.",
-    "My Java Web Programming final project was made using Java, utilizing JSP to add, edit, and update movies along with its information."
+    "My Java Web Programming final project was made using Java, utilizing JSP to add, edit, and update movies along with its information.",
+    "This Java Android App began as a final personal college project at CVTC and has since been enhanced with a ton more features, soon to release on the Google Play Store."
   ];  
   
   if($imageLinkArray.length >= 3) {
